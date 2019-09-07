d7b8dc1b65b5bf62401e5339da62e13deade2c56
mitchb95 <mitchell.bonner@gmail.com>
2019-09-06 21:56:23 -0400

    Making a recruiting cog, moving commands out of bot script

--- robo-albert.py
+++ robo-albert.py
@@ -1,6 +1,6 @@
-import aiohttp
 import configparser
-import json
+
+import recruiting
 
 from discord.ext import commands
 
@@ -10,37 +10,7 @@ config.read('config.ini')
 TOKEN = config['default']['token']
 
 bot = commands.Bot(command_prefix='?')
-
-@bot.command()
-async def team_croot_rank(ctx, *args):
-    message = 'Invalid syntax.  ?team_croot_rank <year> <team>'
-    if len(args) <= 1:
-        await ctx.send(message)
-        return
-
-    year = args[0]
-
-    try:
-        int(year)
-    except ValueError:
-        await ctx.send(message)
-        return
-
-    team = ' '.join(args[1:])
-
-    url = f'https://api.collegefootballdata.com/recruiting/teams?year={year}&team={team}'
-    async with aiohttp.ClientSession() as session:
-        raw_response = await session.get(url)
-        response = await raw_response.text()
-        response = json.loads(response)
-        if not response:
-            message = 'No results.'
-        else:
-            rank = response[0]['rank']
-            points = response[0]['points']
-            message = f'Recruiting rank for {team} in {year}: {rank} ({points})'
-
-        await ctx.send(message)
+bot.add_cog(recruiting.Recruiting(bot))
 
 @bot.command()
 async def ping(ctx):
