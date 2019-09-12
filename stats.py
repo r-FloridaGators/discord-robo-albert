c2e079f6b4b94c145365f2292064c2d3ec5f8efa
mitchb95 <mitchell.bonner@gmail.com>
2019-09-11 22:14:37 -0400

    Correcting post season and not sending post season results if none exist

--- stats.py
+++ stats.py
@@ -72,11 +72,9 @@ class Stats(commands.Cog):
             response = await raw_response.text()
             response = json.loads(response)
             if not response or len(response) is 0:
-                message = 'No results.'
-                await ctx.send(message)
                 return
             else:
-                message = f'**{team} in {year} (Regular season)**:\n'
+                message = f'**{team} in {year} (Post season)**:\n'
                 for value in response:
                     away_team = value['away_team']
                     home_team = value['home_team']
